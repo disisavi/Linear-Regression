944328e1437994027fe531962061085b7274a1ab
Abhijeet Mishra <disisavi@localhost.localdomain>
2019-09-30 18:53:26 -0400

    Code Cleanup

--- RegressionEngine.py
+++ RegressionEngine.py
@@ -1,5 +1,4 @@
 from typing import List
-import sys
 
 
 class regressionEngine:
@@ -14,15 +13,12 @@ class regressionEngine:
 
     def getslice(self, index: int) -> List[int]:
         returnList = [self.x[0][index], self.x[1][index], self.x[2][index]]
-        # if self.test:
-        #     print("For index {}, slice is {}".format(index, returnList))
         return returnList
 
     def calculateH(self, thetalist: List, index: int) -> int:
         h = 0
         slice = self.getslice(index)
         for index, theta in enumerate(thetalist):
-            # print(slice[index])
             h += theta * slice[index]
         return h
 
@@ -36,39 +32,28 @@ class regressionEngine:
             h = self.calculateH(thetalist, i)
             hlist.append(h)
             jtheta.append(self.calculateJ(h, self.y[i]))
-            # if self.test:
-            #     print("For iteration {}, H = {} and J = {}".format(i, h, jtheta[i]))
 
         jtheta = sum(jtheta)
-        # print("Jtheta = ", jtheta)
         jtheta = jtheta / (2 * len(self.y))
         return jtheta, hlist
 
     def doGradientDecent(self) -> List[int]:
-        thetalist = [[100, 100, 100]]
-        # thetalist.append([[0, 0, 0]])
+        thetalist = [[1000, 1000, 1000]]
+
         if self.test:
             print("************ Starting gradient descent. :)")
         for theta in thetalist:
             for k in range(self.maxiterations):
-                # todo
-                # 1. Allow multiple theta's to be initialised from the start and then compare
-                # the final result among all of them to check we reached True Minima
                 if self.test:
                     print("Iteration Number ", k)
                 newthetalist = []
                 h = [(self.calculateH(theta, i) - self.y[i]) for i in range(len(self.y))]
-                # if self.test:
-                #     print("The hi - yi = ", h)
 
                 olderror = self.calculateJTheta(theta)[0]
                 for i in range(len(theta)):
                     hx = [h[j] * self.x[i][j] for j in range(len(h))]
                     hx = sum(hx) / len(self.y)
                     newTheta = theta[i] - self.alpha * hx
-                    # if self.test:
-                    #     print("\tNew HX ", hx)
-                    #     print("NewTheta = ", newTheta)
                     newthetalist.append(newTheta)
 
                 newerror = self.calculateJTheta(newthetalist)[0]
@@ -84,3 +69,7 @@ class regressionEngine:
 
         print("K = ", k)
         return newthetalist
+
+# todo
+# 1. Allow multiple theta's to be initialised from the start and then compare
+# the final result among all of them to check we reached True Minima
